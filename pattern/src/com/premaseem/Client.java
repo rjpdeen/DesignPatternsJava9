4d3b0adf98fe1fb4a76352b4a7f5cf3256cc3a4a
Ron Deen <rondeen@air-van-ron.home>
2022-01-08 12:49:39 +0100

    8

--- pattern/src/com/premaseem/Client.java
+++ pattern/src/com/premaseem/Client.java
@@ -2,8 +2,8 @@ package com.premaseem;
 
 import java.util.Scanner;
 
-/*
-@author: Aseem Jain
+/*@author: Aseem Jain
+
 @title: Design Patterns with Java 9
 @link: https://premaseem.wordpress.com/category/computers/design-patterns/
 @copyright: 2018 Packt Publication
